b8ea61fd59b4e71a15562fcd21000f464e3ceeaa
xiyoushuaige <xiyoushuaige@126.com>
2016-04-13 23:01:03 +0800

    add project1
    add project1

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by 西邮帅哥 on 16/4/7.
+//  Copyright © 2016年 xiyoushuaige. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var numberDisplayLabel: UILabel!
+    var number = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        numberDisplayLabel.text = "\(number)"
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func resetButtonPressed(sender: UIBarButtonItem) {
+        number = 0
+        numberDisplayLabel.text = "\(number)"
+    }
+
+    @IBAction func tapButtonPressed(sender: UIButton) {
+        number += 1
+        numberDisplayLabel.text = "\(number)"
+        }
+    }
+    
+
+
